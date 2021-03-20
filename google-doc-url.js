556f3b7f95d4331f9974c15090b10492dbf5085a
tourdetravoy <dooganb@gmail.com>
2021-03-20 11:00:54 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1J2XUXFuCcelJEmRaEJ4z0pkJOVAZ2ix1llHp3lH1kFs/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDmuFb9LZyfgKREHzrj8MF6EzjG2_CrGqo';
